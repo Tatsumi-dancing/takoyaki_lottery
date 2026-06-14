544b6ae5303627272e792df7edb40a350d326c2e
Tatsumi-dancing <lzuoteng555@gmail.com>
2026-06-14 19:41:49 +0900

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,7 @@ module ApplicationHelper
       color: "000",
       shape_rendering: "crispEdges",
       module_size: 8,
+      offset: 8,
       standalone: true,
       use_path: true
     ).html_safe
